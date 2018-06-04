117e3a7ad509715c8241b480b016cba722e4adb7
meelvin182 <sokolov@phystech.edu>
2018-06-05 00:35:20 +0300

    dsf
    sad

--- src/main/java/com/example/demo/helloController.java
+++ src/main/java/com/example/demo/helloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class helloController {
     @RequestMapping("/")
     public String helloWorld(){
-        return "dun-duk";
+        return "Hello dun-duk";
     }
 }
