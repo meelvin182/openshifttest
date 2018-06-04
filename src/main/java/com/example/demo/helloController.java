0e2dd4efb3b21f8794390658f14e9299effa468d
meelvin182 <sokolov@phystech.edu>
2018-06-05 00:33:54 +0300

    qwe

---
+++ src/main/java/com/example/demo/helloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sokolov
+ */
+@RestController
+public class helloController {
+    @RequestMapping("/")
+    public String helloWorld(){
+        return "dun-duk";
+    }
+}
